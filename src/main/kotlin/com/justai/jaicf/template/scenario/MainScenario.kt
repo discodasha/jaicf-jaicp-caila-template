08627645c5068e7714b28e90d5c1dac595e69d5d
Dasha Serdyuk <d.serdyuk@just-ai.com>
2021-08-27 18:52:02 +0300

    add handling one caila intent

--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -4,44 +4,10 @@ import com.justai.jaicf.activator.caila.caila
 import com.justai.jaicf.builder.Scenario
 
 val mainScenario = Scenario {
-    state("start") {
-        activators {
-            regex("/start")
-            intent("Hello")
-        }
-        action {
-            reactions.run {
-                image("https://media.giphy.com/media/ICOgUNjpvO0PC/source.gif")
-                sayRandom(
-                    "Hello! How can I help?",
-                    "Hi there! How can I help you?"
-                )
-                buttons(
-                    "Help me!",
-                    "How are you?",
-                    "What is your name?"
-                )
-            }
-        }
-    }
-
-    state("bye") {
-        activators {
-            intent("Bye")
-        }
-
-        action {
-            reactions.sayRandom(
-                "See you soon!",
-                "Bye-bye!"
-            )
-            reactions.image("https://media.giphy.com/media/EE185t7OeMbTy/source.gif")
-        }
-    }
 
     state("smalltalk", noContext = true) {
-        activators {
-            anyIntent()
+        globalActivators {
+            intent("Birthday")
         }
 
         action(caila) {
@@ -50,9 +16,6 @@ val mainScenario = Scenario {
     }
 
     fallback {
-        reactions.sayRandom(
-            "Sorry, I didn't get that...",
-            "Sorry, could you repeat please?"
-        )
+        reactions.say("Вот и встретились два одиночества...")
     }
 }
